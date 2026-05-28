957acb43c05d6706598592e45189a83be273ee4e
Reiniel Fernandez Cruz <git@reinielfc.dev>
2026-05-28 17:20:07 -0400

    Refactor QDBus client implementation to use dbus package and improve method calls

--- qdbus/qdbus.go
+++ qdbus/qdbus.go
@@ -1,61 +1,98 @@
 package qdbus
 
 import (
-	"fmt"
-	"os/exec"
-	"strings"
+	"github.com/godbus/dbus/v5"
 )
 
-type QDBusClient struct {
-	svc, path, iface string
+const (
+	propertiesInterface = "org.freedesktop.DBus.Properties"
+)
+
+type QDBusInterfaceClient struct {
+	conn      *dbus.Conn
+	svcName   string
+	objPath   string
+	ifaceName string
 }
 
-func NewQDBusClient(svc, path, iface string) *QDBusClient {
-	return &QDBusClient{svc, path, iface}
+func NewQDBusInterfaceClient(conn *dbus.Conn, svcName, objPath, ifaceName string) *QDBusInterfaceClient {
+	return &QDBusInterfaceClient{
+		conn:      conn,
+		svcName:   svcName,
+		objPath:   objPath,
+		ifaceName: ifaceName,
+	}
 }
 
-func (c *QDBusClient) cmd(method string, args ...any) (*exec.Cmd, error) {
-	path, err := exec.LookPath("qdbus6")
+func (c *QDBusInterfaceClient) callInterface(iface, method string, flags dbus.Flags, args ...any) (*dbus.Call, error) {
+	call := c.object().Call(iface+"."+method, flags, args...)
+	if call.Err != nil {
+		return nil, call.Err
+	}
+	return call, nil
+}
+
+func (c *QDBusInterfaceClient) callGetter(flags dbus.Flags, property string) (*dbus.Call, error) {
+	return c.callInterface(propertiesInterface, "Get", flags, c.ifaceName, property)
+}
+
+func (c *QDBusInterfaceClient) callMethod(method string, flags dbus.Flags, args ...any) (*dbus.Call, error) {
+	return c.callInterface(c.ifaceName, method, flags, args...)
+}
+
+func (c *QDBusInterfaceClient) object() dbus.BusObject {
+	return c.conn.Object(c.svcName, dbus.ObjectPath(c.objPath))
+}
+
+// typed calls and properties
+
+func (c *QDBusInterfaceClient) boolProperty(property string, flags dbus.Flags) (bool, error) {
+	return typedProperty[bool](c, property, flags)
+}
+
+func (c *QDBusInterfaceClient) voidCall(method string, flags dbus.Flags, args ...any) error {
+	_, err := c.callMethod(method, flags, args...)
+	return err
+}
+
+func (c *QDBusInterfaceClient) uintCall(method string, flags dbus.Flags, args ...any) (uint32, error) {
+	return typedCall[uint32](c, method, flags, args...)
+}
+
+func (c *QDBusInterfaceClient) stringCall(method string, flags dbus.Flags, args ...any) (string, error) {
+	return typedCall[string](c, method, flags, args...)
+}
+
+func (c *QDBusInterfaceClient) stringListCall(method string, flags dbus.Flags, args ...any) ([]string, error) {
+	return typedCall[[]string](c, method, flags, args...)
+}
+
+func typedProperty[T any](c *QDBusInterfaceClient, property string, flags dbus.Flags) (T, error) {
+	call, err := c.callGetter(flags, property)
 	if err != nil {
-		return nil, fmt.Errorf("qdbus6 not found: %w", err)
+		var zero T
+		return zero, err
 	}
-
-	cmdArgs := []string{c.svc, c.path, c.iface + "." + method}
-	for _, arg := range args {
-		cmdArgs = append(cmdArgs, fmt.Sprintf("%v", arg))
+	var value T
+	err = call.Store(&value)
+	if err != nil {
+		var zero T
+		return zero, err
 	}
-
-	return exec.Command(path, cmdArgs[1:]...), nil
+	return value, nil
 }
 
-func (c *QDBusClient) run(method string, args ...any) error {
-	if cmd, err := c.cmd(method, args...); err != nil {
-		return err
-	} else if err := cmd.Run(); err != nil {
-		return fmt.Errorf("failed to call method %s: %w", method, err)
-	} else {
-		return nil
+func typedCall[T any](c *QDBusInterfaceClient, method string, flags dbus.Flags, args ...any) (T, error) {
+	call, err := c.callMethod(method, flags, args...)
+	if err != nil {
+		var zero T
+		return zero, err
 	}
-}
-
-func (c *QDBusClient) output(method string, args ...any) (string, error) {
-	if cmd, err := c.cmd(method, args...); err != nil {
-		return "", err
-	} else if output, err := cmd.Output(); err != nil {
-		return "", fmt.Errorf("failed to call method %s: %w", method, err)
-	} else {
-		return string(output), nil
+	var result T
+	err = call.Store(&result)
+	if err != nil {
+		var zero T
+		return zero, err
 	}
-}
-
-func (c *QDBusClient) outputLines(method string, args ...any) ([]string, error) {
-	if output, err := c.output(method, args...); err != nil {
-		return nil, err
-	} else {
-		lines := strings.Split(output, "\n")
-		for i := range lines {
-			lines[i] = strings.TrimSpace(lines[i])
-		}
-		return lines, nil
-	}
+	return result, nil
 }
